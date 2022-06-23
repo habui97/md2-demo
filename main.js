09956d61b68747902a805ffc7ec1cd41401fd469
Ha Bui <ha.hoangbui97@gmail.com>
2022-06-23 12:35:59 -0500

    finhished demo

---
+++ main.js
@@ -0,0 +1,65 @@
+// let backpack = []; 
+
+// backpack.push('pokeball')
+// backpack.push('potion')
+// backpack.push('pokedollars')
+
+// // removes first item
+// // backpack.shift() 
+// backpack.splice(0,1)
+
+// let foundItem = 'waterstone';
+// backpack.push(foundItem)
+
+// backpack.pop()
+
+// let itemCount = backpack.length; 
+// // console.log(itemCount)
+
+// backpack.push('great ball', 'antidote', 'revive')
+
+// remove 2 last items 
+// let satchel = backpack.splice(2, 3)
+// console.log(satchel)
+
+// using for loop with arrays 
+// for (let i = 0; i < backpack.length; i++){
+//     console.log(backpack[i])
+// }
+
+// if(backpack.length >= 3){
+//     console.log('hit 3 statement')
+//     for(let i = 0; i < 3; i++){
+//         console.log(backpack[i])
+//         // console.log(backpack.length)
+//     }
+// } else {
+//     console.log('hit else')
+//     for(let i = 0; i < backpack.length; i++){
+//         console.log(backpack[i])
+//     }
+// }
+
+
+// console.log(backpack)
+
+
+let guessMe = 54
+
+while (guessMe < 100) {
+    console.log('-------', guessMe);
+    if (guessMe % 4 === 0 || guessMe % 5 === 0) {
+        guessMe += 25
+        console.log('guessMe is divisible by 4 or 5, added 25');
+    } else if (guessMe % 3 === 0) {
+        guessMe -= 27
+        console.log('guessMe is divisible by 3, subtracted 27');
+    } else {
+        guessMe += 3
+        console.log('guessMe hit else, added 3')
+    }
+    guessMe += 22
+    console.log(`Added 22. guessMe is now ${guessMe}`)
+}
+
+console.log('final value', guessMe)
